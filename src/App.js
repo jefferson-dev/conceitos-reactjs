0ac73564ae15d3cb1045ac3fb208a4cc6f4f99b7
Jefferson Marques <jefferson@iMac-de-Jefferson.local>
2020-10-27 02:49:52 -0300

    Teste concluido

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const newProject = await api.post('repositories', {
+      title: `Teste ${Date.now()}`,
+      owner: 'Jefferson'
+    })
+
+    const add = newProject.data;
+
+    setRepositories([...repositories, add])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(project => project.id !== id))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      {repositories.map(project => <li key={project.id}>{project.title}
+        <button onClick={() => handleRemoveRepository(project.id)}>
+          Remover
+        </button>
+        </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
